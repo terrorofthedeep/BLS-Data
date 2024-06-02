2fe440be6c120d9ebc6b6009f2f3b2b6c9dfa491
terrorofthedeep <lucasmpierce@gmail.com>
2024-06-02 17:34:31 -0400

    minor changes

--- app.py
+++ app.py
@@ -4,9 +4,19 @@ import prettytable
 
 
 if __name__=='__main__':
+    #Getting parameters for API URL
+    keyFile = open("registrationkey.txt", 'r')
+    registrationKey = keyFile.read()
+
+
+    #url = 'https://api.bls.gov/publicAPI/v2/timeseries/data/?' + registrationKey
+
+    #Code prefix seems to be OE, UM or US?
+    #ex. OEUM001058000000029203313 ("Annual median wage for Nuclear Medicine Technologists in All Industries in Albany-Schenectady-Troy, NY")
+
     #Retrieving data
     headers = {'Content-type': 'application/json'}
-    data = json.dumps({"seriesid": ['CUUR0000SA0','SUUR0000SA0'],"startyear":"2011", "endyear":"2014"})
+    data = json.dumps({"seriesid": ['CUUR0000SA0','SUUR0000SA0'],"startyear":"2023", "endyear":"2023", "registrationkey": registrationKey})
     p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
     json_data = json.loads(p.text)
 
